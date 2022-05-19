ffe1c55a455eb9b591a936f0831ceddb0e8dfd86
uNFixed7 <kamalov7r@gmail.com>
2022-05-19 08:25:09 +0300

    Initial Commit

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by antonfrolov on 31.03.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var profileBirthday: UILabel!
+    @IBOutlet weak var profileCity: UILabel!
+    @IBOutlet weak var profileInfo: UITextView!
+}
+
---
+++ Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by antonfrolov on 31.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
